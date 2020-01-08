828a7eea1eae6644977e503093054a06cbe0c0d5
zmpoper <51334314@qq.com>
2020-01-08 17:52:33 +0800

    首页组件完成

--- README.md
+++ README.md
@@ -2,5 +2,24 @@
 ## 制作首页App组件
 1. 完成Header区域，使用的是Mint-UI中的Header组件
 2. 制作底部的Tabbar区域，使用的是MUI的Tabbar.html
+   + 在制作购物车 小图标的时候，操作会多一些
+   + 先把 扩展图标的css样式拷贝到项目中
+   + 拷贝 扩展字体库 ttf 文件，到项目中
+   + 为购物车小图标，添加如下样式：mui-icon mui-icon-extra mui-icon-extra-cart
 3. 要在中间区域放置一个router-view来展示路由匹配的组件
 
+## 改造 tabbar 为 router-link
+
+## 设置路由高亮
+
+## 点击 tabbar 中的路由链接，展示对应的路由组件
+
+## 制作首页轮播图
+
+## 加载首页轮播数据
+1. 获取数据，如何获取数据，使用vue-resource
+2. 使用vue-resource 的 this.$http.get 获取数据
+3. 获取到的数据，要保存到data身上
+4. 使用v-for循环渲染每个item项
+
+## 改造九宫格样式
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,61 @@
 <template>
   <div class="app-container">
-      <!-- 顶部header区域 -->
-      <mt-header fixed title="Vue项目"></mt-header>
+    <!-- 顶部header区域 -->
+    <mt-header fixed title="Vue项目"></mt-header>
+    
 
+    <!-- 中间的路由 router-view--->
+	<transition >
+    <router-view></router-view>
+	</transition>
 
-      <!-- 中间的路由 router-view--->
-
-
-      <!-- 底部Tabbar区域 -->
-      <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
-				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-				<span class="mui-tab-label">消息</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">通讯录</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">设置</span>
-			</a>
-		</nav>
-      <h1>
-          这是App组件
-      </h1>
+    <!-- 底部Tabbar区域 -->
+    <nav class="mui-bar mui-bar-tab">
+      <router-link class="mui-tab-item" to="/home">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/shopcar">
+        <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+          <span class="mui-badge">0</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/search">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
   </div>
 </template>
 <script>
 //按需导入Toast组件
-
-
 </script>
 // node-sass@^4.0.0 
 // fibers@>= 3.1.0
 
 <style lang="scss" scoped>
-.app-container{
-  padding-top: 40px; 
- }
+.app-container {
+  padding-top: 40px;
+  overflow-x: hidden;//解决页面切换时的问题header和taber会暂时错乱
+}
+
+.v-enter{
+	opacity: 0;
+	transform: translateX(100%);
+}
+.v-leave-to{
+	opacity: 0;
+	transform: translateX(-100%);
+	position: absolute;//解决页面切换时上下漂的问题
+}
+.v-enter-active,
+.v-leave-active{
+	transition: all 0.5s ease
+}
 
 </style>
